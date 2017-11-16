34e6bc10be3970f0e988d7e5721c44ef71c4fe3f
Sebastian Galkin <paraseba@gmail.com>
2017-11-16 12:19:11 -0200

    Add shell.nix file
    If you are using nix, see
    
       https://nixos.org/nixpkgs/manual/#users-guide-to-the-haskell-infrastructure
    
    otherwise, it's perfectly safe to ignore this file

---
+++ shell.nix
@@ -0,0 +1,36 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, cmdargs, mwc-random, primitive, split
+      , stdenv, transformers
+      }:
+      mkDerivation {
+        pname = "pituitary";
+        version = "0.0.1";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [
+          base cmdargs mwc-random primitive split transformers
+        ];
+        executableHaskellDepends = [
+          base cmdargs mwc-random primitive split transformers
+        ];
+        description = "Reimplementation of R. Bertram works.";
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
