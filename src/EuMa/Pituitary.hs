4ee0b04e2fca538b0effd82e807f1c873acc4b46
Sebastian Galkin <paraseba@gmail.com>
2017-11-16 13:25:27 -0200

    Fix performance
    Generous use of inlining

--- src/EuMa/Pituitary.hs
+++ src/EuMa/Pituitary.hs
@@ -1,6 +1,16 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module EuMa.Pituitary where
+module EuMa.Pituitary
+  ( Comp
+  , Env(In)
+  , lengthSpikes
+  , lengthSpikesUpTo
+  , simulate
+
+-- we don't really need to export dotVar, but because of inlineing we *need* to export it
+-- to avoid a big performance penalty
+  , dotVar
+  ) where
 
 import Control.Monad.Trans.Reader (ReaderT, ask)
 import System.Random.MWC (Gen)
@@ -8,6 +18,7 @@ import System.Random.MWC.Distributions (standard)
 import Control.Monad.Primitive (PrimState, PrimMonad)
 
 import EuMa.Types
+
 
 data Env m = In Parameters Global (Gen (PrimState m))
 type Comp m a = ReaderT (Env m) m a
@@ -27,6 +38,7 @@ data State = State { stIca    :: Double
 wiener :: (PrimMonad m) => Double -> Double -> Gen (PrimState m) -> m Double
 wiener x dt gen = (* step) <$>  standard gen
   where step = x * (dt ** 0.5)
+{-# INLINABLE wiener #-}
 
 -- State variables are a function of model variables
 state :: (PrimMonad m) => Variables Double -> Comp m State
@@ -45,6 +57,7 @@ state  Variables{..} = do
                , stIleak  = gl*(varV - vl)
                , stInoise = eta
                }
+{-# INLINABLE state #-}
 
 -- set of equations that describe the dynamics of model variables in terms of state variables
 dotVar :: (PrimMonad m) => Variables Double -> Comp m (Variables Double)
@@ -55,6 +68,9 @@ dotVar v@Variables{..} = do
                       , varn  = (stninf - varn)/taun
                       , varf  = (stfinf - varf)/taubk
                       , varCa = -fc*(alpha*stIca + kc*varCa) }
+-- notice we need to force inlineing for this one, ghc seems to have the wrong heuristic
+-- without inlining here performance degrades 10x
+{-# INLINE dotVar #-}
 
 -------------- Euler–Maruyama method to estimate the stochastic differential equation (SDE) ----------
 eulerStep :: (PrimMonad m) => Variables Double -> Comp m (Variables Double)
@@ -63,15 +79,18 @@ eulerStep v = do
         dv <- dotVar v
         return $ (+) . (*stepSize) <$> dv <*> v
      -- return $ liftA2 (+) v (fmap (stepSize*) dv) -- the same
+{-# INLINABLE eulerStep #-}
 
 iterateMn :: Monad m => Int -> (a -> m a) -> a -> m [a]
 iterateMn n f x0 = do
     new <- f x0
     if n==1 then return [new] else (new:) <$> iterateMn (n-1) f new
     -- (new:) <$> iterateM f new -- nicer, but Random.MWC is not lazy
+{-# INLINABLE iterateMn #-}
 
 simulate :: (PrimMonad m) => Int -> Variables Double -> Comp m [Variables Double]
 simulate n = iterateMn n eulerStep
+{-# INLINABLE simulate #-}
 
 lengthSpikes :: (PrimMonad m) => Int -> Variables Double -> Double -> Comp m [Int]
 lengthSpikes  n x0 th = comph' n x0 [0] where
@@ -85,6 +104,7 @@ lengthSpikes  n x0 th = comph' n x0 [0] where
             where v = varV new
     if m==1 then return (clean hhh) else comph' (m-1) new hhh
        where clean yy@ys = if head ys == 0 then ys else yy
+{-# INLINABLE lengthSpikes #-}
 
 lengthSpikesUpTo :: (PrimMonad m) => Int -> Variables Double -> Double -> Comp m [Int]
 lengthSpikesUpTo n x0 th = comph' x0 [0] where
@@ -98,3 +118,4 @@ lengthSpikesUpTo n x0 th = comph' x0 [0] where
             where v = varV new
     if length hhh == n then return (clean hhh) else comph' new hhh
        where clean yy@(ys) = if  head ys ==0 then ys else yy
+{-# INLINABLE lengthSpikesUpTo #-}
