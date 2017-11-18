ff17ed8d0fc8249159f49447dfc301c820f14b86
Sebastian Galkin <paraseba@gmail.com>
2017-11-18 00:59:15 -0200

    Improve profiling configuration
    cabal configure --enable-profiling
    
    cabal run pituitary-prof -- multi --numCurves 5 --numThreads 8
    
    Then look at *.prof for time profiling.
    
    For memory profiling, after running the executable do
    
    hp2ps -e8in -c pituitary-prof.hp
    
    and inspect the generated ps file
    
    In my run, 83% of the time was spent in uniform (inside MWC package)

---
+++ release.nix
@@ -0,0 +1,24 @@
+let
+  config = {
+    packageOverrides = pkgs: rec {
+      haskellPackages = pkgs.haskellPackages.override {
+        overrides = haskellPackagesNew: haskellPackagesOld: rec {
+
+          mkDerivation = args: haskellPackagesOld.mkDerivation (args // {
+            enableLibraryProfiling = true;
+          });
+
+          pituitary =
+            haskellPackagesNew.callPackage ./default.nix {
+            };
+        };
+      };
+    };
+  };
+
+  pkgs = import <nixpkgs> { inherit config; };
+
+in
+  rec {
+    pituitary = pkgs.haskellPackages.pituitary;
+}
