2863d4e04263fa35454a8b50253869880937bd97
Sebastian Galkin <paraseba@gmail.com>
2017-11-18 00:59:15 -0200

    Improve profiling configuration
    cabal configure --enable-profiling
    
    cabal run pituitary-prof -- multi --numCurves 5 --numThreads 8
    
    Then look at *.prof for time profiling.
    
    For memory profiling, after running the executable do
    
    hp2ps -e8in -c pituitary-prof.hp
    
    and inspect the generated ps file
    
    In my run, 83% of the time was spent in uniform (inside MWC package)

---
+++ default.nix
@@ -0,0 +1,23 @@
+{ mkDerivation, async, base, bytestring, cassava, mwc-random
+, optparse-applicative, pipes, pipes-bytestring, pipes-concurrency
+, pipes-csv, primitive, split, stdenv, transformers, vector
+}:
+mkDerivation {
+  pname = "pituitary";
+  version = "0.0.1";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    async base bytestring cassava mwc-random optparse-applicative pipes
+    pipes-bytestring pipes-concurrency pipes-csv primitive split
+    transformers vector
+  ];
+  executableHaskellDepends = [
+    async base bytestring cassava mwc-random optparse-applicative pipes
+    pipes-bytestring pipes-concurrency pipes-csv primitive split
+    transformers vector
+  ];
+  description = "Reimplementation of R. Bertram works.";
+  license = stdenv.lib.licenses.mit;
+}
